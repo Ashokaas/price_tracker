8f5869db5ebf48af6b59a09e37ef6db00c448855
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2022-10-12 14:52:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from requests import get
 from bs4 import BeautifulSoup
 from colorama import Fore
-from win10toast_click import ToastNotifier
-from webbrowser import open
+from threading import ThreadError
+import schedule
+import telebot
+import time
 
-NOMBRE_DE_PRODUIT_A_AFFICHER = 5
+
+API_KEY = # Votre clef api (str)
+CHAT_ID = # Votre CHAT_ID (int)
+bot = telebot.TeleBot(API_KEY)
+
+
+
 
 # -- Requête pour récupérer une page
 def info_site(url):
@@ -20,41 +28,39 @@ def info_site(url):
     return site
 
 
-def rechercher_produit(produit:str, uniquement_magasin:list=None):
-    # Si l'utilisateur met directement le lien
-    try:
-        # On met l'id du produit en int
-        int(produit)
-        # On effectue une requête pour récupérer la page du produit
-        lien = f"https://ledenicheur.fr/product.php?p={produit}"
-        site = info_site(lien)
+def rechercher_produit(produit:str, uniquement_magasin:list=None, nombre_de_produits_a_afficher:int=5):
+    # On effectue une requête pour obtenir une liste des produits proche du souhait de l'utilisateur
+    site = info_site(f'https://ledenicheur.fr/search?search={produit}')
+    # On défini les listes contenants le nom et le lien des produits
+    liste_nom = []
+    liste_lien = []
+    # Pour chaque enfant dans le tableau contenant la liste des produits
+    for child in site.find('ul', class_ = 'ListUl-sc-xo0c91-0 fTCido').findChildren():
+        # On récupère le lien et le nom
+        lien = child.find('a', class_ = 'InternalLink-sc-t916l0-1 hYGtTZ ProductLink-sc-ezay95-0 cXuLZZ')
+        nom = child.find('span', class_ = 'Text--bzqghn kBBoFI titlesmalltext')
+        print(nom)
+        # Si le nom existe et qu'il n'a jamais été trouvé
+        if nom and nom.text not in liste_nom:
+            liste_nom.append(nom.text)
+        # Si le lien existe et qu'il n'a jamais été trouvé
+        if lien and lien['href'] not in liste_lien:
+            liste_lien.append(lien['href'])
+    # On affiche la liste à l'utilisateur
+    propostiion = ''
+    print(liste_nom)
+    for i in range(nombre_de_produits_a_afficher):
+        propostiion += (f'{i+1} : {liste_nom[i]}\n')
+    
+    return propostiion, liste_lien
+    
 
-    # Sinon
-    except:
-        # On effectue une requête pour obtenir une liste des produits proche du souhait de l'utilisateur
-        site = info_site(f'https://ledenicheur.fr/search?search={produit}')
-        # On défini les listes contenants le nom et le lien des produits
-        liste_nom = []
-        liste_lien = []
-        # Pour chaque enfant dans le tableau contenant la liste des produits
-        for child in site.find('ul', class_ = 'ListUl-sc-xo0c91-0 fTCido').findChildren():
-            # On récupère le lien et le nom
-            lien = child.find('a', class_ = 'InternalLink-sc-t916l0-1 hYGtTZ ProductLink-sc-ezay95-0 cXuLZZ')
-            nom = child.find('span', class_ = 'Text--bzqghn kBBoFI titlesmalltext')
-            # Si le nom existe et qu'il n'a jamais été trouvé
-            if nom and nom.text not in liste_nom:
-                liste_nom.append(nom.text)
-            # Si le lien existe et qu'il n'a jamais été trouvé
-            if lien and lien['href'] not in liste_lien:
-                liste_lien.append(lien['href'])
-        # On affiche la liste à l'utilisateur
-        for i in range(NOMBRE_DE_PRODUIT_A_AFFICHER):
-            print(f'{i+1} : {liste_nom[i]}')
-        # L'utilisateur choisi le produit
-        selectionner_produit = int(input('Selectionnez le bon produit : '))
-        # Requête pour avoir la page du produit selectionné
-        lien = f"https://ledenicheur.fr{liste_lien[selectionner_produit-1]}"
-        site = info_site(lien)
+
+def afficher_produit(nb, liste_lien, uniquement_magasin):
+
+    # Requête pour avoir la page du produit selectionné
+    lien = f"https://ledenicheur.fr{liste_lien[nb-1]}"
+    site = info_site(lien)
 
     # On affiche le titre du produit
     titre_produit = site.find('h1', class_ = 'Text--bzqghn bhbQJh h2text Title-sc-16x82tr-2 daHeSK')
@@ -63,34 +69,70 @@ def rechercher_produit(produit:str, uniquement_magasin:list=None):
     ul = site.find('ul', class_ = 'PriceList-sc-wkzg9v-0 fbrkVc')
 
     liste_magasin = []
-    liste_tout = []
+    liste_tout = {}
     for child in ul.findChildren():
         prix = child.find('span', class_ = 'Text--bzqghn ddqBq bodysmalltext PriceLabel-sc-lboeq9-0 jCcYnj StyledPriceLabel-sc-k40pbc-0 kQerWx')
         magasin = child.find('span', class_ = 'StoreInfoTitle-sc-bc2k22-1 idSYNT')
         # Si le prix et le magasin existe et si le magasin n'a pas encore été trouvé
         if prix and magasin and magasin.text not in liste_magasin:
-            liste_magasin.append(magasin.text)
-            liste_tout.append(f'{magasin.text} : {prix.text}')
+            if True in [(text in magasin.text) for text in uniquement_magasin]:
+                liste_magasin.append(magasin.text)
+                liste_tout[magasin.text] = prix.text
+            
+    return liste_tout
     
-    liste_tout_2 = ''
-    for item in liste_tout:
-        if uniquement_magasin != None:
-            for magasin_1 in uniquement_magasin:
-                if magasin_1 in item:
-                    print(item)
-                    liste_tout_2 += '\n' + item
-        else:
-            print(item)
-            liste_tout_2 += '\n' + item
-    print(liste_tout_2)
 
-    ToastNotifier().show_toast(title=titre_produit.text, msg=liste_tout_2, icon_path='', callback_on_click=lambda:open(lien))
+    
 
    
 
 
 
-if __name__ == '__main__':
-    # 1er argument : (str)(obligatoire) Le produit ou l'id du produit que vous voulez
-    # 2ème argument : (list)(facultatif) Pour avoir le meilleur prix dans les enseignes que vous voulez
-    rechercher_produit(produit='5727010', uniquement_magasin=['Amazon', 'Fnac', 'Leclerc', 'Cultura'])
+# 1er argument : (str)(obligatoire) Le produit ou l'id du produit que vous voulez
+# 2ème argument : (list)(facultatif) Pour avoir le meilleur prix dans les enseignes que vous voulez
+#rechercher_produit(produit='splatoon 3', uniquement_magasin=['Amazon', 'Fnac', 'Leclerc', 'Cultura', 'Auchan'], nombre_de_produits_a_afficher=5)
+
+
+
+@bot.message_handler(commands=['isactive'])
+def is_active(message):
+    is_active_msg = bot.send_message(chat_id=CHAT_ID, text="Yes\n\n(Suppression dans 3 secondes)")
+    time.sleep(3)
+    bot.delete_message(chat_id=CHAT_ID, message_id=is_active_msg.id)
+    
+    
+    
+@bot.message_handler(commands=['prix'])
+def prix(message):
+    print(message.text)
+    produit = message.text[6:]
+    print(produit)
+
+    proposition, liste_lien = rechercher_produit(produit)
+    print(proposition)
+    
+    bot.send_message(chat_id=CHAT_ID, text=proposition)
+    
+    @bot.message_handler()
+    def prix2(message):
+        liste_finale = afficher_produit(int(message.text), liste_lien, ['Amazon', 'Fnac', 'Leclerc', 'Cultura', 'Auchan'])
+        print(liste_finale)
+        message_a_envoyer = ''
+        for magasin, prix in zip(list(liste_finale.keys()), list(liste_finale.values())):
+            message_a_envoyer += f'{magasin} : {prix}\n'
+        
+        print(message_a_envoyer)
+        bot.send_message(chat_id=CHAT_ID, text=str(message_a_envoyer))
+        
+        @bot.message_handler(commands=['follow'])
+        def follow(message):
+            while True:
+                if liste_finale != afficher_produit(int(message.text), liste_lien, ['Amazon', 'Fnac', 'Leclerc', 'Cultura', 'Auchan']):
+                    bot.send_message(chat_id=CHAT_ID, text='Le prix a changé')
+                
+        
+    
+    
+    
+    
+bot.infinity_polling()
